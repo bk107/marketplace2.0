c8e4b650ce1a197622bd5134a4e639863247915b
bk107 <bastian.kaltdorf@gmail.com>
2021-08-28 21:43:25 +0200

    removed console log

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,6 @@ export default function Home() {
     const [products, setProducts] = useState([])
 
     const fetchProducts = () => {
-        console.log("process.env.REACT_APP_JSON_SERVER", process.env.REACT_APP_MIDDLEWARE_SERVER)
         fetch(process.env.REACT_APP_MIDDLEWARE_SERVER + '/getAllProductsWithPrices')
             .then(res => res.json())
             .then(response => {
